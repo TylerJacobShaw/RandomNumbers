096998e6bad78bc47a3520804a89d591db3b802f
Tyler Shaw <tylerjacobshaw@gmail.com>
2019-05-27 18:46:04 -0500

    init commit

---
+++ RandomNumberGenerator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using RandomNumberGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomNumberGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBlock.Text = "Orignal List:";
+            textBlock1.Text = "Sorted List:";
+        }
+        Tree MyTree;
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            Random ran = new Random();
+            int num;
+            int num1 = int.Parse(Number1.Text);
+            string generatedNumbers = "";
+            string treestring = "";
+            num = ran.Next(0, 100);
+            generatedNumbers = generatedNumbers + num.ToString().PadLeft(3);
+            MyTree = new Tree(num);
+            int n = Convert.ToInt32(num1);
+            for (int i = 1; i < n; i++)
+            {
+                num = ran.Next(0, 100);
+                generatedNumbers = generatedNumbers + num.ToString().PadLeft(3);
+                MyTree.AddRc(num);
+            }
+            MyTree.Print(null, ref treestring);
+            textBlock.Text = $"Orignal List: {generatedNumbers}";
+            textBlock1.Text = $"Sorted List: {treestring}";
+        }
+    }
+}
---
+++ RandomNumberGenerator/Models/Tree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator.Models
+{
+    public class Tree
+    {
+        Node top;
+        public Tree()
+        {
+            top = null;
+        }
+        public Tree(int initial)
+        {
+            top = new Node(initial);
+        }
+        public void Add(int value)
+        {
+            if (top == null)
+            {
+                Node node = new Node(value);
+                top = node;
+                return;
+            }
+            Node currentNode = top;
+            bool isAdded = false;
+            do
+            {
+                if (value < currentNode.value)
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.left = newNode;
+                        isAdded = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                if (value >= currentNode.value)
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.right = newNode;
+                        isAdded = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+
+            } while (!isAdded);
+        }
+        public void AddRc(int initial)
+        {
+            AddR(ref top, initial);
+        }
+        private void AddR(ref Node node, int value)
+        {
+            if (node == null)
+            {
+                Node newNode = new Node(value);
+                node = newNode;
+                return;
+            }
+            if (value < node.value)
+            {
+                AddR(ref node.left, value);
+            }
+            if (value >= node.value)
+            {
+                AddR(ref node.right, value);
+            }
+        }
+        public void Print(Node node, ref string message)
+        {
+            if (node == null)
+            {
+                node = top;
+            }
+            if (node.left != null)
+            {
+                Print(node.left, ref message);
+                message = message + node.value.ToString().PadLeft(3);
+            }
+            else
+            {
+                message = message + node.value.ToString().PadLeft(3);
+            }
+            if (node.right != null)
+            {
+                Print(node.right, ref message);
+
+            }
+        }
+    }
+}
